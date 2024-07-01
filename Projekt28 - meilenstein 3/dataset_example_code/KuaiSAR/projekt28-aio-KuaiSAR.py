6661af4d6f0e928b9b2365035e3013b746ad3976
TEOVI <150669035+TEOVI@users.noreply.github.com>
2024-07-01 13:34:18 +0200

    Update projekt28-aio-KuaiSAR.py
    results round_to=6

--- Projekt28 - meilenstein 3/dataset_example_code/KuaiSAR/projekt28-aio-KuaiSAR.py
+++ Projekt28 - meilenstein 3/dataset_example_code/KuaiSAR/projekt28-aio-KuaiSAR.py
@@ -297,7 +297,7 @@ def main():
         np.exp(trace.posterior), var_names=[
             "Intercept",
             "days_since_signup",
-        ]
+        ],round_to=6
     )
 
     exp_coeff_table
@@ -315,7 +315,7 @@ def main():
             "1|day_index_sigma",
             "1|user_sigma",
             "variety_alpha"
-        ]
+        ],round_to=6
     )
 
     coeff_table
